aca07288cbb0bde4b652a19126da71c917b147ee
jackiecrazy <jackiecrazyhi@gmail.com>
2022-03-22 14:07:44 -0700

    gotta...

--- src/main/java/jackiecrazy/combatcircle/utils/MotionUtils.java
+++ src/main/java/jackiecrazy/combatcircle/utils/MotionUtils.java
@@ -4,7 +4,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.vector.Vector3d;
 
 public class MotionUtils {
-    public static Vector3d convertToFacing(LivingEntity mover, Vector3d moveVec) {
-        return moveVec.rotateYaw(GeneralUtils.rad(mover.rotationYaw));
+    public static Vector3d convertToFacing(LivingEntity mover, Vector3d moveVec) {//FIXME does this work?
+        return moveVec.rotateYaw(GeneralUtils.rad(mover.rotationYaw)).rotatePitch(GeneralUtils.rad(mover.rotationPitch));
     }
 }
